1b6f76531c3d30a62f4ae0c1d5848c685a7c272e
Rishav Raj <rixhavraj@gmail.com>
2025-05-04 03:28:49 +0530

    commit

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express() //import express from "express"
 const mongoose = require('mongoose')
 const Path = require('path')
-const port = 3000
+require('dotenv').config()
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/login');
+const port = process.env.PORT || 3000;
+mongoose.connect(process.env.MONGODB_URI);
+
 const db = mongoose.connection;
 db.once('open', ()=>{
   console.log("mongo db connectes succesfully!!!")
@@ -21,29 +23,30 @@ const userSchema = new mongoose.Schema({
 
 const users = mongoose.model("data", userSchema)
 
+app.use('/', express.static(Path.join(__dirname)))
+
 app.get('/', (req, res) => {
   res.sendFile(Path.join(__dirname, 'index.html'));
 });
 
 
+app.use('/shayari', express.static(Path.join(__dirname, '../shayari')))
 
-app.post('/', async(req, res)=>{
+app.post('/shayari', async (req, res) => {
   const { Username, Password } = req.body;
-  const user = new users({
-    Username, Password
-  });
-  try{
-  await user.save();
-  console.log("saved to db", user);
-  res.send("Form submitted successfully!!!");
-  } catch(error){
+  const user = new users({ Username, Password });
+
+  try {
+    await user.save();
+    console.log("saved to db", user);
+    res.redirect('shayari')
+  } catch (err) {
     console.log("Error saving to db", err);
     res.status(500).send("error saving data");
   }
 });
 
 
-
 app.listen(port, () => {
-  console.log(`Listening at ${port}`)
+  console.log(`Listening at port ${port}`)
 })
